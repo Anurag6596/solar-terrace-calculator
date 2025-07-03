4e4a17adb83ac4bc9c678196bf540b1fe1274669
ianuraggavhane <77688394+Anurag6596@users.noreply.github.com>
2025-07-03 15:37:10 +0530

    initai changes

--- server/app.js
+++ server/app.js
@@ -26,19 +26,6 @@ app.use(cors({
 
 app.use(express.json());
 
-// Handle preflight OPTIONS requests for all routes
-app.options('*', cors({
-  origin: function(origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    } else {
-      return callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-}));
-
 connectDB();
 
 app.use('/api/save-calculation', saveCalculationRoute);
